f2a77730bda3f10b2a2f7d47543e6b677c7701e5
Ilir Sheraj <sherajilir@gmail.com>
2026-08-18 20:00:28 +0200

    added sets

---
+++ Chapter2_DataTypes/sets.py
@@ -0,0 +1,44 @@
+# set is mutable, frozenset is immutable
+# empty set
+small_primes = set()
+print(small_primes)
+small_primes.add(2)
+small_primes.add(3)
+small_primes.add(5)
+print(small_primes)
+
+small_primes.add(1)
+print(small_primes)
+small_primes.remove(1)
+print(small_primes)
+
+print(3 in small_primes)
+print(4 in small_primes)
+print()
+print("Add 3 to small primes and see what happens")
+small_primes.add(3)
+print(small_primes)
+
+bigger_primes = set([5, 7, 11, 13])
+print(bigger_primes)
+
+print("Union of small and bigger primes")
+print(small_primes | bigger_primes)
+print("Intersection of small and bigger primes")
+print(small_primes & bigger_primes)
+
+print("Difference between small and bigger primes")
+print(small_primes - bigger_primes)
+print("Difference between bigger and small primes")
+print(bigger_primes - small_primes)
+print()
+
+print("Lets work with frozen sets")
+small_primes = frozenset([2, 3, 5, 7])
+bigger_primes = frozenset([5, 7, 11])
+try:
+    small_primes.add(11)
+except AttributeError:
+    print("AttributeError")
+
+print(small_primes & bigger_primes)
