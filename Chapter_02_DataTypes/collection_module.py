60194f77a0408634923fdb548365cc174a37bb2f
Ilir Sheraj <sherajilir@gmail.com>
2026-09-08 00:21:35 +0200

    finished 2

--- Chapter_02_DataTypes/collection_module.py
+++ Chapter_02_DataTypes/collection_module.py
@@ -1,4 +1,5 @@
-from collections import namedtuple, defaultdict
+from collections import namedtuple, defaultdict, ChainMap
+from enum import Enum
 
 Vision = namedtuple("Vision", ["left", "right"])
 vision = Vision(left=9.5, right=8.8)
@@ -16,8 +17,46 @@ print(vision.left)
 print(vision.right)
 print(vision.combined)
 print("-" * 50)
+
 # Default dictionary
 d = defaultdict(int) # value 0
 print(d)
 d["age"] += 1
 print(d)
+print("-" * 50)
+print()
+
+# Explore ChainMap
+default_connection = {"host": "localhost", "port": 4567}
+connection = {"port": 5678}
+print(f"The default connection is {default_connection}")
+print(f"The connection is {connection}")
+
+conn = ChainMap(connection, default_connection)
+print(conn["port"])
+print(conn["host"])
+print(conn.maps)
+
+conn["host"] = "packtpub.com"
+print(conn.maps)
+
+del conn["port"]
+print(conn.maps)
+print(conn["port"])
+print(dict(conn))
+print("-" * 50)
+print()
+
+# Explore Enum
+class TrafficLight(Enum):
+    GREEN = 1
+    YELLOW = 2
+    RED = 4
+
+print(TrafficLight.GREEN)
+print(TrafficLight.GREEN.value)
+print(TrafficLight(1))
+print(TrafficLight(4))
+
+
+
