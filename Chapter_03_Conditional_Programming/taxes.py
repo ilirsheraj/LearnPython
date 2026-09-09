df476cbbf5a9a6ddfa9e887744cd296e8aa3903c
Ilir Sheraj <sherajilir@gmail.com>
2026-09-09 22:35:33 +0200

    conds

---
+++ Chapter_03_Conditional_Programming/taxes.py
@@ -0,0 +1,12 @@
+income = 15_000
+
+if income < 10_000:
+    tax_coefficient = 0.0
+elif income < 30_000:
+    tax_coefficient = 0.2
+elif income < 100_000:
+    tax_coefficient = 0.35
+else:
+    tax_coefficient = 0.45
+
+print(f"You will pay ${tax_coefficient * income} in taxes this year")
