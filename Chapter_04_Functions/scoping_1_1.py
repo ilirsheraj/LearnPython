b2bd0aa0ff0a4651c4316dc3f443e7bf9ef5df88
Ilir Sheraj <sherajilir@gmail.com>
2026-09-25 22:10:07 +0200

    scope

---
+++ Chapter_04_Functions/scoping_1_1.py
@@ -0,0 +1,14 @@
+# define test in the global scope
+test = 0
+
+def my_function():
+    # Define test in the local scope
+    # test = 1
+    print("my_function: ", test)
+
+def main():
+    my_function()
+    print("global: ", test)
+
+if __name__ == "__main__":
+    main()
